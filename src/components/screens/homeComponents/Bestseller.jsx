434ec73da91b89d696a45da9a92d3ea8631a5f16
schhaya <schhaya144@gmail.com>
2024-05-30 12:38:35 +0530

    bestseller changes

--- src/components/screens/homeComponents/Bestseller.jsx
+++ src/components/screens/homeComponents/Bestseller.jsx
@@ -11,28 +11,28 @@ const products = [
     id: 1,
     name: "Peace Lily Plant",
     rating: 4.5,
-    delprice:399,
+    delprice: 399,
     price: 299,
   },
   {
     id: 2,
     name: "Snake Plant",
     rating: 4.2,
-    delprice:499,
+    delprice: 499,
     price: 399,
   },
   {
     id: 3,
     name: "Spider Plant",
     rating: 4.8,
-    delprice:299,
+    delprice: 299,
     price: 199,
   },
   {
     id: 4,
     name: "Spider Plant",
     rating: 4.8,
-    delprice:1499,
+    delprice: 1499,
     price: 1299,
   },
 ];
@@ -41,23 +41,30 @@ function Bestseller() {
   return (
     <div>
       <Container>
-      <div className="heading">
-          <h2><img src={bestSeller} alt="" className="seller-best"/> Bestsellers</h2>
+        <div className="heading">
+          <h2>
+            <img src={bestSeller} alt="" className="seller-best " /> Bestsellers
+          </h2>
         </div>
         <div className="row">
           {products.map((product) => (
-            <div className="col-md-3">
+            <div className="col-6 col-md-3">
               <Card className="border border-0">
                 <a href="" className="nolink">
-                <Badge bg="warning" text="dark">
-                      sale
-                    </Badge>
-                  <div className="textdecor">
-                    
+                  <div className="textdecor position-relative">
                     <img src={bestPicks} alt="" className="bextpick" />
+                    <Badge
+                      bg="warning"
+                      text="dark"
+                      className="position-absolute  rounded-0 text-uppercase p-2 sale-badge"
+                    >
+                      {" "}
+                      sale{" "}
+                    </Badge>
                   </div>
                   <Card.Body className="p-0 m-0">
                     <Card.Title>{product.name}</Card.Title>
+                    <div className="bestProductName pt-2 pb-3"></div>
                     <div className="star-rating">
                       {Array.from({ length: 5 }, (v, i) => (
                         <i
@@ -68,21 +75,29 @@ function Bestseller() {
                         />
                       ))}
                     </div>
-                    <p className="text-success h6"> <del className="text-secondary"><i class="fa-solid fa-indian-rupee-sign point12px"> </i> {product.delprice}</del> From <i class="fa-solid fa-indian-rupee-sign point12px"> </i> {product.price}</p>
+                    <p className="text-success h6">
+                      {" "}
+                      <del className="text-secondary">
+                        <i class="fa-solid fa-indian-rupee-sign point12px"> </i>{" "}
+                        {product.delprice}
+                      </del>{" "}
+                      From{" "}
+                      <i class="fa-solid fa-indian-rupee-sign point12px"> </i>{" "}
+                      {product.price}
+                    </p>
                     <Button className="w-100 btn btn-success rounded-0 text-uppercase">
                       View Product
                     </Button>
                   </Card.Body>
                 </a>
               </Card>
-              
             </div>
           ))}
           <div className="viewall my-5 d-flex justify-content-center">
-              <Button className="px-5 btn btn-success rounded-0 text-uppercase rounded-1">
-                      View All
-                    </Button>
-              </div>
+            <Button className="px-5 btn btn-success rounded-0 text-uppercase rounded-1">
+              View All
+            </Button>
+          </div>
         </div>
       </Container>
     </div>
---
+++ src/components/screens/homeComponents/DealOfTheWee.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function DealOfTheWee() {
+  return (
+    <div>
+      <div className="container">
+        <div class="row">
+            <div className="col-md-6">
+                <div className="img dealImg">
+                    <img src="" alt="" />
+                </div>
+            </div>
+            <div className="col-md-6"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DealOfTheWee
