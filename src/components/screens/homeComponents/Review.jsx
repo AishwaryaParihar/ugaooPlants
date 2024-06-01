dac7654d73ef8dac7bf8e9a3ea6deb8154593e53
aish <toshiparihar121212@gmail.com>
2024-06-01 17:06:02 +0530

    remove owl

--- src/components/screens/homeComponents/Review.jsx
+++ src/components/screens/homeComponents/Review.jsx
@@ -1,33 +1,16 @@
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+
 
 import React from 'react';
-import Slider from 'react-slick';
 
-const settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000
-};
 
-const Carousel = () => {
+
+
+const Review = () => {
   return (
-    <Slider {...settings}>
-      <div>
-        <img src="https://via.placeholder.com/600x400" alt="Image 1" />
-      </div>
-      <div>
-        <img src="https://via.placeholder.com/600x400" alt="Image 2" />
-      </div>
-      <div>
-        <img src="https://via.placeholder.com/600x400" alt="Image 3" />
-      </div>
-    </Slider>
+    <>
+    
+    </>
   );
 };
 
-export default Carousel;
+export default Review;
